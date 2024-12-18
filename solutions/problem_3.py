67ee1ee1dbff087ed42facbd3edbd674bf8f4e5d
Halil Emre <98128335+halilemreakbas@users.noreply.github.com>
2024-12-19 02:52:50 +0300

    Update problem_3.py

--- solutions/problem_3.py
+++ solutions/problem_3.py
@@ -1,1 +1,17 @@
+#The problem asks:
+#The prime factors of 13195 are 5,7,13 and 29
+#What is the largest prime factor of the number 600851475143 ?
 
+#lpf = largest prime factor of a number
+def lpf(n):
+    factor = 2  # start with the smallest prime factor
+    while factor * factor <= n:  # keep going while primefactor^2 doesn't exceed the number
+        if n % factor == 0:  # if n is divisible by the current factor
+            n //= factor  # divide n by the factor
+        else:
+            factor += 1  # move to the next factor
+    return n  # the remaining value of n is the largest prime factor
+
+# asked number
+number = 600851475143
+print(lpf(number))  #so the answer is "6857"
