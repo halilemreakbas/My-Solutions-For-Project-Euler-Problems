d59943eca9fa61e2b4c8fb08a4194d9bcb6f8bb5
Halil Emre <98128335+halilemreakbas@users.noreply.github.com>
2024-12-18 15:37:00 +0300

    Update problem_1.py

--- solutions/problem_1.py
+++ solutions/problem_1.py
@@ -1,2 +1,6 @@
-### The problem asks: 
-### If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.Find the sum of all the multiples of 3 or 5 below 1000. 
+# The problem asks: 
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.Find the sum of all the multiples of 3 or 5 below 1000. 
+# Approaching to the Problem:
+# Step 1: Loop through all numbers from 1 to 999.
+# Step 2: For each number, check if it is divisible by 3 or 5 using the modulus operator (%).
+# Step 3: If the number is divisible by either, add it to the total sum.
